3727cdd98f5923328f5a2d9011d8ba7d3779334a
TuPi <tupi032002@gmail.com>
2023-04-06 10:36:00 +0700

    update html css -Tu

--- giadien/edit/js/head-bot.js
+++ giadien/edit/js/head-bot.js
@@ -17,3 +17,11 @@ itemsliderbar.forEach(function(menu, index){
         menu.classList.toggle("block") // hien thi block
     })
 })
+// Product
+const bigImg = document.querySelector(".product-content-left-big-img")
+const smalImg = document.querySelectorAll(".product-constent-left-smalll-img img")
+smalImg.forEach(function(imgItem,X){
+    imgItem.addEventListener("click", function(){
+        bigImg.src = imgItem.src
+    })
+})
